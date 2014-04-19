7965058a6553c572a6362fb00d0f40b135f3f1b2
Gordon <gtosh4@gmail.com>
2014-04-19 02:56:55 -0700

    Add boostrap script to link the files from the dotfiles directory (assumed to be ~/dotfiles).

---
+++ boostrap.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Link dot files
+ln -s -t ~ \
+~/dotfiles/.zshrc \
+~/dotfiles/.Xresources \
+~/dotfiles/.aliases \
+~/dotfiles/.tmux.conf \
+
+# Cover our X defaults/resources bases
+ln -s .Xresources ~/.Xdefaults
+
+# Emacs init.el
+mkdir ~/.emacs.d
+ln -s ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
+
+# Fonts
+ln -s ~/dotfiles/.fonts .fonts
+
+# Solarized
+git clone https://github.com/altercation/solarized.git ~/solarized
+git clone https://github.com/seebi/dircolors-solarized ~/solarized/dircolors-solarized
