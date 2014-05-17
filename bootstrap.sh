40591d294b1fbb1d4ceba2718e42c32e63730903
Gordon <gtosh4@gmail.com>
2014-05-17 15:44:19 -0700

    Convert absolute '~/dotfiles' to use the dirname of the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
+DOTFILES=$(dirname $0)
+
 # Link dot files
 ln -s -t ~ \
-~/dotfiles/.zshrc \
-~/dotfiles/.Xresources \
-~/dotfiles/.aliases \
-~/dotfiles/.tmux.conf \
-~/dotfiles/.pylintrc
+    ~/dotfiles/.zshrc \
+    $DOTFILES/.Xresources \
+    $DOTFILES/.aliases \
+    $DOTFILES/.tmux.conf \
+    $DOTFILES/.pylintrc
 
 # Link all scripts to bin
 [ ! -d ~/bin ] && mkdir ~/bin
-ln -s -t ~/bin ~/dotfiles/bin/*
+ln -s -t ~/bin $DOTFILES/bin/*
 
 # Cover our X defaults/resources bases
 ln -s .Xresources ~/.Xdefaults
 
 # Emacs init.el
 [ ! -d ~/.emacs.d ] && mkdir ~/.emacs.d
-ln -s -d ~/.emacs.d ~/dotfiles/.emacs.d/*
+ln -s -d ~/.emacs.d $DOTFILES/.emacs.d/*
 
 # Fonts
-ln -s ~/dotfiles/.fonts .fonts
+ln -s $DOTFILES/.fonts .fonts
 
 # Solarized
 [ ! -d ~/.emacs.d/solarized ] && git clone custom-theme-load-path ~/.emacs.d/solarized
@@ -28,4 +30,4 @@ ln -s ~/dotfiles/.fonts .fonts
 
 # Oh My Zsh
 [ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-ln -s ~/dotfiles/gg-clean.zsh-theme ~/.oh-my-zsh/custom/
+ln -s $DOTFILES/gg-clean.zsh-theme ~/.oh-my-zsh/custom/
