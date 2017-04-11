e685fc96ddb775acccdca9aea40fb07de7077857
gtosh4 <gtosh4@gmail.com>
2017-04-11 11:06:55 -0700

    Add nginx mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,6 +17,7 @@
                      lua-mode
                      dockerfile-mode
                      powershell
+                     nginx-mode
                      
                      auto-complete
                      go-autocomplete
