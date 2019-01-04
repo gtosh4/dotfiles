0d5ba027ebc2fdba275200ce221820123e0c7ce2
Gordon Goetz <ggoetz@hyperiontechnologiescorp.com>
2019-01-04 14:43:07 -0800

    Fix go.mod gitignore

--- bin/hyperion_core_docker.sh
+++ bin/hyperion_core_docker.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-[ ! -f $(pwd)/remote-docker.sock ] && ssh -fNT -L "$(pwd)/remote-docker.sock:/var/run/docker.sock" core@10.0.0.10
+cleanup()
+{
+    rm "$HOME/hyperion-docker.sock"
+}
 
-export DOCKER_HOST="unix://$(pwd)/remote-docker.sock"
+if [ ! -f $HOME/hyperion-docker.sock ]; then
+    trap 'cleanup' 0 1 2
+    ssh -NT -L "$HOME/hyperion-docker.sock:/var/run/docker.sock" core@10.0.0.10
+fi
