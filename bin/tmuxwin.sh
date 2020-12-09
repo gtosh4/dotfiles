0663f841beb8e810404a2b2feca5cfd93f32acc6
Gordon Goetz <ggoetz@riotgames.com>
2020-12-09 14:22:45 -0800

    support ssh name

--- bin/tmuxwin.sh
+++ bin/tmuxwin.sh
@@ -1,7 +1,22 @@
-case $1 in
+WIN_NAME=$1
+DIR=$2
+
+case $WIN_NAME in
     sh|bash|zsh)
-        echo "/$(basename $2)"
+        echo "/$(basename $DIR)" 
         ;;
+
+    ssh)
+        # https://github.com/soyuka/tmux-current-pane-hostname/blob/master/scripts/shared.sh
+        cmd=$({
+            pgrep -flaP `tmux display-message -p "#{pane_pid}"`
+            ps -o command -p `tmux display-message -p "#{pane_pid}"`
+        } | xargs -I{} echo {} | grep ssh | sed -E 's/^[0-9]*[[:blank:]]*ssh //')
+        host=$(echo $cmd | awk '{print $NF}'|cut -f2 -d@)
+        echo $host
+        ;;
+
     *)
-        echo $1
+        echo $WIN_NAME
+        ;;
 esac
